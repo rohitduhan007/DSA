d3008b0171d2fba609ce611a27d2ea1a61c07515
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-11 18:08:55 +0530

    Add files via upload

--- pattern_2/sol2/pattern_2.cpp
+++ pattern_2/sol2/pattern_2.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int i,j,k,n;
+    cin >> n;
+    for(i=0 ; i<n ; i++ )
+    {
+        for(j=n-1-i ; j>0 ; j-- )
+        {
+            cout <<" ";
+
+        }
+        for(j=0 ; j<=i ; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
