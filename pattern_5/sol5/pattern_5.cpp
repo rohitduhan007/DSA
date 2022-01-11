5574b553bac129d7b78f121b18467de568df1c7d
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-12 04:31:04 +0530

    Add files via upload

--- pattern_5/sol5/pattern_5.cpp
+++ pattern_5/sol5/pattern_5.cpp
@@ -1,1 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    {
+
+
+
+    int rows=1;
+    while(rows<n)
+    {
+        int space = n-rows ;
+        while(space)
+        {
+            cout<<" ";
+            space--;
+
+        }
+        int col = 1;
+        while(col <= rows)
+        {
+            cout<<"* ";
+            col++;
+        }
+        cout<<endl;
+        rows++;
+
+
+    }
+    }
+    {
+
+         int rows = 1;
+         while(rows<=n){
+
+        //int i=1;
+        int space = 1;
+        while (space < rows)
+        {
+            cout<<" ";
+            space++;
+
+        }
+        //int str = n-rows;
+        int col = 1;
+        while(col <= n-rows+1)
+        {
+            cout<<"* ";
+            col++;
+            //str++;
+
+
+        }
+
+        cout<<endl;
+        rows++;
+         }
+    }
+
+
+
+}
