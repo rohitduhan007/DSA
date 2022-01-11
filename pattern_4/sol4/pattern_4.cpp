723a86bd255fb2e6a98546682b924dc6294f379b
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-12 00:36:49 +0530

    Add files via upload

--- pattern_4/sol4/pattern_4.cpp
+++ pattern_4/sol4/pattern_4.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int row=1;
+    while(row<=n)
+    {
+        int space = n - row;
+        while(space)
+        {
+            cout<<" ";
+            space--;
+
+        }
+        int col = 1;
+        while(col<=row)
+        {
+            cout<<"*"<<" ";
+            col++;
+        }
+         cout<<endl;
+         row++;
+        }
+
+
+    }
+
 
