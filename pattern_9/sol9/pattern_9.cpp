e96549913f04040bde4fbf6179b1539e3fe73b59
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 03:04:33 +0530

    Add files via upload

--- pattern_9/sol9/pattern_9.cpp
+++ pattern_9/sol9/pattern_9.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+int row = n-1;
+ int x=1;
+ int y = row*2-1;
+ for(int out = row ; out>=1 ; out--)
+ {
+     for(int in = 1; in <=row*2; in++)
+     {
+         if(in == x || in == y)
+         {
+             cout<<out;
+         }
+         else
+         {
+             cout<<" ";
+         }
+     }
+     x++;
+     y--;
+     cout<<"\n";
+
+
+
+}
+}
