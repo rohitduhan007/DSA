403f1afc07a3fbf0cef98ff9dc7d6cea96cbb195
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 05:20:19 +0530

    Add files via upload

--- pattern_10/pattern_10.cpp
+++ pattern_10/pattern_10.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+
+    int n;
+
+
+    cout << "Enter the value of n \n";
+    cin >> n;
+
+
+    for (int i = 1; i <= n; i++) {
+
+
+        for (int j = 1; j <= n; j++)
+        {
+            // For i = 1, we print a '$' only in
+            // first and last columns
+            // For i = 2, we print a '$' only in
+            // second and second last columns
+            // In general, we print a '$' only in
+            // i-th and n+1-i th columns
+
+            if (j == i || j == (n + 1 - i))
+                cout << i;
+
+            else
+                cout << " ";
+        }
+
+        // Print a newline before printing the
+        // next row.
+        cout << endl;
+
+
+    }
+    return 0;
+}
