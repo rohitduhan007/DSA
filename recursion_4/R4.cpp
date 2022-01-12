285c4ec52f641a8da1ca398c106e9e74859b434c
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 03:47:50 +0530

    Add files via upload

--- recursion_4/R4.cpp
+++ recursion_4/R4.cpp
@@ -1,1 +1,24 @@
+//first n even no
 
+#include<bits/stdc++.h>
+using namespace std;
+
+void printnat(int n)
+{
+    for(int i=1 ; i<=n ; i++  )
+    {
+    if( i%2 == 0)
+    {
+        cout <<i<<" ";
+    }
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    //cout<< n + printnat(n-1);
+    printnat(n);
+}
