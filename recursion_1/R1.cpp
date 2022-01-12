37bd4943f7921a995467a4d48b574876a21ca084
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 03:21:37 +0530

    Add files via upload

--- recursion_1/R1.cpp
+++ recursion_1/R1.cpp
@@ -1,1 +1,22 @@
+//Sum of first n natural no using recursion
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int addnat(int n)
+{
+    if(n==1)
+    {
+        return 1 ;
+    }
+    return n+addnat(n-1);
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"sum of "<< n <<" natural no is -> " << n+addnat(n-1) << endl;
+}
