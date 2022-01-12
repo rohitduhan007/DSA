94283d01cd47f0ee2b0e3a6ebcdfe6fd21d08da6
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-12 06:04:42 +0530

    Add files via upload

--- pattern_7/sol7/pattern_7.cpp
+++ pattern_7/sol7/pattern_7.cpp
@@ -1,1 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+    {
+
+
+
+    int row = 1;
+    while(row<=n)
+    {
+        int col=1;
+        int count = 5;
+        while(col<=row)
+        {
+            cout<<count;
+            count--;
+            col++;
+
+
+        }
+
+
+        cout<<endl;
+        row++;
+    }
+    }
+    {
+      int row = 1;
+      while(row<=n)
+      {
+
+        int col=1;
+        int count=5;
+        while(col<=n-row+1)
+        {
+            cout<<count;
+            count--;
+            col++;
+        }
+        cout<<endl;
+        row++;
+      }
+    }
+}
