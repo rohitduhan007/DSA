f2d14736e6641b98c62413e973f29a530553f4b7
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 03:36:26 +0530

    Add files via upload

--- recursion_2/R2.cpp
+++ recursion_2/R2.cpp
@@ -1,1 +1,22 @@
+//first n natural no
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printnat(int n)
+{
+    if(n>=1)
+    {
+     printnat(n-1);
+     cout<<n<<" ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    //cout<< n + printnat(n-1);
+    printnat(n);
+}
