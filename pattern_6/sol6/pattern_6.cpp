f81402c7eea983aec4c76cd9a886c83ae730bedf
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-12 05:47:21 +0530

    Add files via upload

--- pattern_6/sol6/pattern_6.cpp
+++ pattern_6/sol6/pattern_6.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+
+    int row = 1 ;
+    while(row <= n)
+    {
+        int space =  n-row ;
+        while(space)
+        {
+            cout<<" ";
+            space--;
+        }
+        int j=1;
+        int var = 2*row-1;
+        //int var = 2*row-1;
+        while(j<=row)
+        {
+            cout<<var;
+            var--;
+            j++;
+            //col++;
+
+        }
+        int start=row-1;
+        while(start)
+        {
+            cout<<start;
+            start--;
+        }
+
+        cout<<endl;
+        row++;
+    }
+}
