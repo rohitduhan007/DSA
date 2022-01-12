8bf1bff25acee08fa14c865622d1fb1c65bb3e92
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 03:43:06 +0530

    Add files via upload

--- recursion_3/R3.cpp
+++ recursion_3/R3.cpp
@@ -1,1 +1,21 @@
+//first n natural no
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printnat(int n)
+{
+    for(int i=n ; i>=0 ; i--)
+    {
+        cout<<i<<" ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    //cout<< n + printnat(n-1);
+    printnat(n);
+}
