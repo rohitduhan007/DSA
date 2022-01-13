f44dd583caa7583dd31c1bcc576e7e31a035bbfe
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 14:15:39 +0530

    Add files via upload

--- pattern_5h/pattern_h5.cpp
+++ pattern_5h/pattern_h5.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std ;
 
+
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i =1 ; i<=n ; i++)
+    {
+        for(int j=1 ; j<=n ; j++)
+        {
+            if(i==j || i==n+1-j  || j==1 || j==n || i==1 || i==n)
+            {
+                cout<< "*" ;
+
+            }
+
+            else
+
+                cout<<" ";
+
+
+        }
+         cout<<endl;
+    }
+}
