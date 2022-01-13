63c8f8ec9b5b248e40d12ce3809f8793f7de3c77
rohitduhan007 <75549864+rohitduhan007@users.noreply.github.com>
2022-01-13 15:20:17 +0530

    Add files via upload

--- pattern_1h/pattern_h1.cpp
+++ pattern_1h/pattern_h1.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;//as a odd no.
+    int x = 1;
+    for (int i = 1 ; i <=n ; i++)
+    {
+        for (int j = 1 ; j <= n ; j++)
+        {
+            if( j<=x || j>=n-x+1 )//|| i == (2*n-1) )//|| i==1 || i==n-1 || j == 1 || j == n-1)
+                cout<<j;
+
+
+            else
+                cout<<" ";
+
+
+        }
+        if(i<=n/2)
+            x++;
+        else
+            x--;
+
+        //x++;
+
+        //for (int j = )
+        cout<<endl;
+    }
+}
